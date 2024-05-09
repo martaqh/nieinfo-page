86729869f9bd35a4ae655214951a56a8f1ea181b
martaqh <marta.k.herma@gmail.com>
2024-05-09 10:49:19 +0100

    Add opacity animanion on page loading

--- src/pages/BasePage.vue
+++ src/pages/BasePage.vue
@@ -5,8 +5,20 @@
 </template>
 
 <style lang="scss" scoped>
+@keyframes transitionIn {
+  from {
+    opacity: 0;
+    transition: rotateX(-10deg);
+  }
+  to {
+    opacity: 1;
+    transition: rotateX(0);
+  }
+}
+
 .base-page {
   max-width: 1200px;
   margin: 0 auto;
+  animation: transitionIn 1.5s;
 }
 </style>
